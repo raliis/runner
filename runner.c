584d53888d10bd76b98de00dfc27ee77faf3e224
rainer <rainer.liis1@gmail.com>
2020-07-14 00:48:42 +0300

    slight polish

--- runner.c
+++ runner.c
@@ -8,25 +8,35 @@ int main(int argc, char **argv)
 				case 0:
 						if (argv[2] && argv[3] && argv[4] && argv[5])
 						{
-								printf ("adding new(id: 56): %.2f(km), %s, %d, %d(kcal)\n", atof(argv[2]), argv[3], atoi(argv[4]), atoi(argv[5]));
+								printf ("Adding new(id: 56): %.2f(km), %s, %d, %d(kcal)\n", atof(argv[2]), argv[3], atoi(argv[4]), atoi(argv[5]));
 						}
 						else
 						{
-								printf ("Missing some arguments, recheck the command\n");
+								printf ("Missing some arguments, cant add entry\n");
 						}
 						break;
 				case 1:
 						if (argv[2])
 						{
-								printf ("Removing run with id %d\n", atoi(argv[2]));
+								printf ("Removing entry with id %d\n", atoi(argv[2]));
 						}
 						else
 						{
-								printf ("Missing id, cant remove run\n");
+								printf ("Missing id, cant remove entry\n");
+						}
+						break;
+				case 2:
+						if (argv[2])
+						{
+								printf ("Launching editor to change entry\n");
+						}
+						else
+						{
+								printf ("Missing id, cant edit entry\n");
 						}
 						break;
 				default:
-						printf ("usage: runner add [dist(float)] [duration(str)], [heart rate(int)], [cals(int)]\n");
+						printf ("Usage: runner add [dist(float)] [duration(str)], [heart rate(int)], [cals(int)]\n");
 						break;
 		}
 		
